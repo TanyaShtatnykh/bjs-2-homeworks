1d7aa52bfe00ecae8cb8255d419702b88be90e5a
Tanya Shtatnykh <tanya.shtatnykh@gmail.com>
2021-09-25 17:21:32 +0700

    First commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,27 @@
 function solveEquation(a, b, c) {
+  'use strict';
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let d = b ** 2 - 4 * a * c;
+
+  if (d < 0) {
+    arr = [];
+    return arr;
+  } else if (d === 0) {
+    let x = -b / (2 * a);
+    arr = [x];
+    return arr;
+  } else if (d > 0) {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr = [x1, x2];
+    return arr;
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+  'use strict';
   let totalAmount;
 
-  // код для задачи №2 писать здесь
-
+  
   return totalAmount;
 }
