0b8b1c7970bc4cb2c14a1d8f62e3cd34e5101362
Tanya Shtatnykh <tanya.shtatnykh@gmail.com>
2021-10-02 17:43:57 +0700

    Arrays first commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,11 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  let result = arr1.length === arr2.length && arr1.every((v, i) => arr2[i] === v);
+  return result;
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  let resultArr = arr.filter(item => item > 0)
+  .filter(item => item % 3 === 0)
+  .map(item => item * 10);
+  return resultArr;
 }
