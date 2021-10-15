445074f57222c2a2f6a13e23e5cf314c21c7442c
Tanya Shtatnykh <tanya.shtatnykh@gmail.com>
2021-10-15 20:16:12 +0700

    First commit objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,30 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+}
+}
+Student.prototype.addMarks = function(...mark) {
+  this.marks = mark;
+}
+
+Student.prototype.getAverage = function() {
+  return this.marks.reduce((a, b) => (a + b)) / this.marks.length;
+}
+
+Student.prototype.exclude = function(reason) {
+  delete Student.prototype.setSubject;
+  delete Student.prototype.addMarks;
+  this.excluded = reason;
+}
